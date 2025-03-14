b9bd8b9299f88ab145cd3e2d0878ee09c6530917
Jhonsese <jhoan.sebastian575@gmail.com>
2025-03-13 19:19:36 -0500

    V-1.0

---
+++ src/main/java/com/project1/demospring/DemospringApplication.java
@@ -0,0 +1,20 @@
+package com.project1.demospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class DemospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringApplication.class, args);
+	}
+
+	@GetMapping("/hola")
+	public String holaMundo() {
+		return "hola-mundo";
+	}
+}
